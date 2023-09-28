f4d24ee2896f021890fd89425036ff9fe6b670b0
AkiraZeZero <kiara.ocasio07@gmail.com>
2023-09-28 18:22:44 -0400

    calculator buttons and state

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {useState, setState} from "react";
+import '../stylesheets/Calculator.css';
+// import calculate from '../logic/calculate';
 
-function Calculator(){
-    return(
-        <div><h1>Calculator</h1></div>
-    )
-}
+const Calculator = () => {
 
-export default Calculator
+    const [state, setState] = useState({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  
+    // const handleClick = (e) => {
+    //   setState((state) => calculate(state, e.target.textContent));
+    // };
+
+//   onClick={handleClick}
+    
+  const { total, next, operation } = state;
+  
+    const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+  
+    return (
+      <>
+        <section className="cal-container">
+          <h2 data-testid="subtitle">
+            Lets do some
+            {' '}
+            <i> maths!</i>
+          </h2>
+          <div className="calculator">
+            <div className="container">
+              <div className="display" data-testid="display">
+                {total}
+                {operation}
+                {next}
+              </div>
+              <div className="keys">
+                {buttons.map((button) => (
+                  <button type="button" className="btn"  key={button} data-testid={button}>{button}</button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+  
+      </>
+    );
+  };
+  
+  export default Calculator;
