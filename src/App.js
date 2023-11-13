5849a99c657b9145726d2fd070f75b9bd40c616a
AkiraZeZero <kiara.ocasio07@gmail.com>
2023-11-13 17:13:57 -0500

    replacing NavBar with Slideshow

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Routes, Route, Navigate, Link} from "react-router-dom";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
+import { Slide } from 'react-slideshow-image';
 import LandingPg from "./Components/LandingPg/LandingPg";
 import Calculator from "./Components/Calculator/Calculator";
-import NavBar from "./Components/NavBar/NavBar";
-import "./App.css";
+import Slider from "./Components/Slider/Slider";
+
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
         <div className="App">
-          <NavBar/>
-             <br/>
-             <h1 className="title">
-          HOME
+          <h1 className="title">
+            Wireframes
           </h1>
-          <h3 className="author">
-          By: Kiara Ocasio-Rosado
-          </h3>
-            <Routes>
-            {/* <Route exact path="/" element={<LandingPg />} /> */}
-            {/* <Route exact path="/" element={<Calculator />} /> */}
+          <div className="slideShowBox">
+            <button></button>
+            <div className="slideShow">
+              
+            </div>
+            <button></button>
+          </div>
+      {/* <Router>
+          <Routes>
+            <Route />
+            <Route exact path="/" element={<Calculator />} />
           </Routes>
-          
-          
+      </Router> */}
         </div>
-      </Router>
     );
   }
 }
---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import "./Slider.css";
+import {Fade, Zoom, Slide} from "react-slideshow-image"
+import cat from "../../Images/calculator.png"
+
+const slideImg = [
+  {
+    image: cat,
+    caption: "Calculator"
+  },
+  {
+    caption: "To-Do-List"
+  }
+]
+
+export default function Slider() {
+  return (
+    <div></div>
+  )
+}
+
